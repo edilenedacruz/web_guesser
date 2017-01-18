b1b0c39995a0d12d17efbc6da64769d13c2941fa
Edilene <edilene_cruz@yahoo.com.br>
2017-01-18 09:14:07 -0700

    complete without limiting or cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,16 +5,16 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  message, color = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color,}
 end
 
 def check_guess(guess)
   case
-  when guess > (SECRET_NUMBER + 5) then "Way too high!"
-  when guess > SECRET_NUMBER then "Too high!"
-  when guess < (SECRET_NUMBER - 5) then "Way too low!"
-  when guess < SECRET_NUMBER then "Too low!"
-  when guess == SECRET_NUMBER then "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
+  when guess > (SECRET_NUMBER + 5) then message = "Way too high!", color = "#FF1612"
+  when guess > SECRET_NUMBER then message = "Too high!", color = "#FFC5CC"
+  when guess < (SECRET_NUMBER - 5) then message = "Way too low!", color = "#FF1612"
+  when guess < SECRET_NUMBER then message = "Too low!", color = "#FFC5CC"
+  when guess == SECRET_NUMBER then message = "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}", color = "#21E809"
   end
 end
